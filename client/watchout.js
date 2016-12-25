dadc023990d75fa6261f63c6640377606ce5f59b
SherryH <enjoy.wind7rain@gmail.com>
2016-12-25 17:17:01 +1300

    Enabled enemies to appear at a random position every second

--- client/watchout.js
+++ client/watchout.js
@@ -13,27 +13,36 @@ var gameBoard = d3.select('.board .mouse').append('svg').attr('height', gameOpti
 console.log(gameBoard.size());
 
 // create enemies. all enemies have .enemy class
-var enemyData = [ ...Array(gameOptions.nEnemies).keys()].map(function(d, i) {
-  return {
-    id: i,
-    x: Math.random() * gameOptions.width,
-    y: Math.random() * gameOptions.height
-  };
-});
+var makeEnemyData = function () {
+  return [ ...Array(gameOptions.nEnemies).keys()].map(function(d, i) {
+    return {
+      id: i,
+      x: Math.random() * gameOptions.width,
+      y: Math.random() * gameOptions.height
+    };
+  });
+};
+var enemyData = makeEnemyData();
 console.log(enemyData);
 
+//bind the enemy data to selection, update enemy id on update
+var enemySel = gameBoard.selectAll('.enemy').data(enemyData, function(d, i) { return d.id; });
+//enter the selections to be rendered
+enemySel.enter().append('circle').attr('class', 'enemy').attr('cx', function(d, i) { return d.x; }).attr('cy', function(d, i) { return d.y; }).attr('r', 10).attr('fill', 'yellow');
 
 
 //-------- Function to update enemy positions on canvas --------
-var updateEnemy = function () {
+var updateEnemy = function (enemyData) {
   //bind the enemy data to selection, update enemy id on update
-  var enemySel = gameBoard.selectAll('.svg .enemy').data(enemyData, function(d, i) { return d.id; });
-  //enter the selections to be rendered
-  enemySel.enter().append('circle').attr('class', 'enemy').attr('cx', function(d, i) { return d.x; }).attr('cy', function(d, i) { return d.y; }).attr('r', 10).attr('fill', 'yellow');
-  //update the enemy position every second
-
-  console.log(enemySel.size());
+  var enemySel = gameBoard.selectAll('.enemy').data(enemyData);
+  enemySel.attr('id', function(d) { return d.id; })
+  .attr('cx', d => d.x)
+  .attr('cy', d => d.y );
+  console.log(enemySel);
 };
 
-updateEnemy();
 
+// updateEnemy(makeEnemyData());
+setInterval(function() {
+  updateEnemy(makeEnemyData());
+},1000);
